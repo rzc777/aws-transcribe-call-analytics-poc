de81d37fa0ed3facc8563f7946521bb85b18c5c8
rzc777 <51310793+rzc777@users.noreply.github.com>
2026-05-10 18:52:37 +0800

    Add WireGuard Terraform main config

---
+++ aws-wireguard-vpn/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "wireguard" {
+  name_prefix = "wireguard-vpn-"
+  description = "WireGuard VPN SG"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 51820
+    to_port     = 51820
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 51821
+    to_port     = 51821
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
