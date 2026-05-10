23a93abea8192b4c57ddd1681d545b323f845c80
rzc777 <51310793+rzc777@users.noreply.github.com>
2026-05-10 19:23:13 +0800

    Complete WireGuard EC2 Terraform resources

--- aws-wireguard-vpn/main.tf
+++ aws-wireguard-vpn/main.tf
@@ -2,29 +2,52 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_ami" "ubuntu_arm64" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_security_group" "wireguard" {
-  name_prefix = "wireguard-vpn-"
-  description = "WireGuard VPN SG"
+  name_prefix = "${var.project_name}-"
+  description = "WireGuard VPN security group"
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.admin_cidr_blocks
   }
 
   ingress {
-    from_port   = 51820
-    to_port     = 51820
+    description = "WireGuard UDP"
+    from_port   = var.wireguard_port
+    to_port     = var.wireguard_port
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 51821
-    to_port     = 51821
+    description = "wg-easy Web UI"
+    from_port   = var.web_ui_port
+    to_port     = var.web_ui_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.admin_cidr_blocks
   }
 
   egress {
@@ -32,5 +55,46 @@ resource "aws_security_group" "wireguard" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-sg"
+    Project = var.project_name
+  }
+}
+
+resource "aws_instance" "wireguard" {
+  ami                         = data.aws_ami.ubuntu_arm64.id
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = [aws_security_group.wireguard.id]
+  associate_public_ip_address = true
+  user_data_replace_on_change = true
+
+  user_data = templatefile("${path.module}/user_data.sh.tpl", {
+    wg_password     = var.wg_easy_password
+    wg_default_dns  = var.wg_default_dns
+    wireguard_port  = var.wireguard_port
+    web_ui_port     = var.web_ui_port
+  })
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp3"
+    encrypted   = true
+  }
+
+  tags = {
+    Name    = "${var.project_name}-ec2"
+    Project = var.project_name
+  }
+}
+
+resource "aws_eip" "wireguard" {
+  instance = aws_instance.wireguard.id
+  domain   = "vpc"
+
+  tags = {
+    Name    = "${var.project_name}-eip"
+    Project = var.project_name
   }
 }
