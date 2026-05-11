52fb0cfda745202a4c7d21fd5c5123033e2ca213
rzc777 <51310793+rzc777@users.noreply.github.com>
2026-05-11 23:35:45 +0800

    Switch WireGuard VPN default region back to Tokyo

--- aws-wireguard-vpn/variables.tf
+++ aws-wireguard-vpn/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region for the VPN server."
   type        = string
-  default     = "ap-east-1"
+  default     = "ap-northeast-1"
 }
 
 variable "project_name" {
