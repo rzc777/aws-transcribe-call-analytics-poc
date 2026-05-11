561a15ad2c199697b5389b9e227c8840d039eec5
rzc777 <51310793+rzc777@users.noreply.github.com>
2026-05-11 13:40:08 +0800

    Fix WireGuard EIP reassociation permissions and source dest check

--- aws-wireguard-vpn/main.tf
+++ aws-wireguard-vpn/main.tf
@@ -83,6 +83,26 @@ resource "aws_iam_role_policy_attachment" "ssm_managed" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy" "eip_reassociate" {
+  name = "${var.project_name}-eip-reassociate"
+  role = aws_iam_role.ssm.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:AssociateAddress",
+          "ec2:DescribeAddresses",
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "ssm" {
   name = "${var.project_name}-ssm-profile"
   role = aws_iam_role.ssm.name
@@ -94,15 +114,18 @@ resource "aws_instance" "wireguard" {
   vpc_security_group_ids      = [aws_security_group.wireguard.id]
   iam_instance_profile        = aws_iam_instance_profile.ssm.name
   associate_public_ip_address = true
+  source_dest_check           = false
   user_data_replace_on_change = true
 
   user_data = templatefile("${path.module}/user_data.sh.tpl", {
-    wg_host        = aws_eip.wireguard.public_ip
-    wg_dns         = var.wg_dns
-    wg_cidr        = var.wg_cidr
-    wg_server_ip   = var.wg_server_ip
-    wg_client_ip   = var.wg_client_ip
-    wireguard_port = var.wireguard_port
+    aws_region        = var.aws_region
+    eip_allocation_id = aws_eip.wireguard.id
+    wg_host           = aws_eip.wireguard.public_ip
+    wg_dns            = var.wg_dns
+    wg_cidr           = var.wg_cidr
+    wg_server_ip      = var.wg_server_ip
+    wg_client_ip      = var.wg_client_ip
+    wireguard_port    = var.wireguard_port
   })
 
   root_block_device {
@@ -116,7 +139,10 @@ resource "aws_instance" "wireguard" {
     Project = var.project_name
   }
 
-  depends_on = [aws_iam_role_policy_attachment.ssm_managed]
+  depends_on = [
+    aws_iam_role_policy_attachment.ssm_managed,
+    aws_iam_role_policy.eip_reassociate
+  ]
 }
 
 resource "aws_eip_association" "wireguard" {
